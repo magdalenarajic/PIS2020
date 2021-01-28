b72a24bd90d2eaad40e72d641653745b0a19711b
magdalenarajic <57672483+magdalenarajic@users.noreply.github.com>
2021-01-28 19:12:31 +0100

    Add files via upload

--- kino/index.php
+++ kino/index.php
@@ -29,7 +29,7 @@ include "config.php";
 <!---NavigationBar--->
 <section id="section-nav-bar">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#section-nav-bar"><img src="img/logo.png" ></a>
+                
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
                 </button>
