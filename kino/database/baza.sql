c0e7d211c5ff97e31d6c5aa8156c9cd5d69374e8
magdalenarajic <57672483+magdalenarajic@users.noreply.github.com>
2021-02-15 16:56:32 +0100

    Add files via upload

--- kino/database/baza.sql
+++ kino/database/baza.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 07, 2021 at 07:53 PM
+-- Generation Time: Feb 15, 2021 at 04:55 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.2.27
 
@@ -44,16 +44,11 @@ CREATE TABLE `bookingtable` (
 --
 
 INSERT INTO `bookingtable` (`bookingID`, `movieName`, `bookingTheatre`, `bookingType`, `bookingDate`, `bookingTime`, `bookingFName`, `bookingLName`, `bookingPNumber`) VALUES
-(25, '1917', 'VIP', '2d', '0000-00-00', '00:00:15', 'Ana', 'Raji?', '063752001'),
-(26, 'Joker', 'Velika', 'imax', '0000-00-00', '00:00:12', 'Antonija', 'Peso', '003859102546'),
-(34, 'Kapetanica Marvel', 'Velika', '3d', '0000-00-00', '12:00:00', 'helena', 'zeko', '222222'),
-(39, 'Gospoda', 'Mala', '3d', '2021-02-16', '16:00:00', 'Ivan', 'Dzakula', '87463532'),
-(40, '1917', 'Velika', '2d', '2021-02-09', '12:00:00', 'Ivan', 'Jozic', '+986764875'),
-(41, 'Joker', 'Velika', '2d', '2021-02-03', '17:00:00', 'Tea', 'Rajić', '+6576535'),
-(42, 'Brzi i zestoki 9', '28', '28', '0000-00-00', '00:00:28', 'Tea', 'Dzakula', '+6576537897'),
-(45, 'Joker', '31', '3D', '2021-02-23', '22:33:00', 'Magdalena', 'Tolj', '0635487972'),
-(46, '1917', '34', '2D', '2021-02-15', '21:34:00', 'Ana', 'Rajic', '+6576537897'),
-(47, 'Joker', '31', '3D', '2021-02-23', '22:33:00', 'Ana', 'Tolj', '87463532');
+(52, 'Joker', 'A2', '4D', '2021-02-17', '22:52:00', 'Ivan', 'Dzakula', '0635487972'),
+(53, '1917', 'Mala', '2D', '2021-02-15', '21:34:00', 'Kristina', 'Arezina', '063524187'),
+(54, 'Kapetanica Marvel', 'VIP', '3D', '2021-02-15', '21:34:00', 'Tea', 'Dzakula', '+986764'),
+(55, 'Gospoda', 'Mala', '3D', '2021-02-23', '22:34:00', 'Tea', 'Marulic', '87463532'),
+(56, 'Cudo u celiji br 7', 'Mala', '4D', '2021-02-24', '16:00:00', 'Helena', 'Zeko', '0635487972');
 
 -- --------------------------------------------------------
 
@@ -130,7 +125,7 @@ INSERT INTO `movietable` (`movieID`, `movieImg`, `movieTitle`, `movieGenre`, `mo
 (12, 'img/1917.jpg', '1917', 'Kriminalistički', 113, '2020-01-23', 'Guy Ritchie', 'Matthew McConaughey, Charlie Hunnam, Michelle Dockery'),
 (14, 'img/opsjednutost.jpg', 'Opsjednutost', 'Horor', 94, '2020-01-30', ' Floria Sigismondi', ' Mackenzie Davis, Finn Wolfhard, Brooklynn Prince'),
 (15, 'img/midnight.jpg', 'Ponoćno nebo', 'Drama, Fantazija, Sci-fi', 118, '2020-12-23', 'George Clooney', ' George Clooney, Felicity Jones, David Oyelowo'),
-(21, 'img/movie-poster-1.jpg', 'Brzi i zestoki 9', 'Akcijski', 123, '2021-03-03', 'Justin Lin', ' Mackenzie Davis, Finn Wolfhard, Brooklynn Prince');
+(28, 'img/7.jpg', 'Cudo u celiji br 7', 'Drama, Kriminalistički', 132, '2020-01-19', 'Mehmet Ada Öztekin', 'Aras Bulut İynemli,Nisa Sofiya Aksongur,Deniz Baysal');
 
 -- --------------------------------------------------------
 
@@ -160,43 +155,8 @@ INSERT INTO `prikazivanje` (`idPrikazivanje`, `IDm`, `movieTitle`, `dvorana`, `d
 (33, 3, 'Gospoda', 'Mala', '2021-02-23', '22:34:00', '3D'),
 (34, 12, '1917', 'Mala', '2021-02-15', '21:34:00', '2D'),
 (35, 15, 'Ponoćno nebo', 'VIP', '2021-02-17', '21:35:00', 'IMAX'),
-(36, 2, 'Joker', 'A2', '2021-02-17', '22:52:00', '4D');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `vrijemefilm`
---
-
-CREATE TABLE `vrijemefilm` (
-  `movieID` int(11) NOT NULL,
-  `vrijeme` time NOT NULL,
-  `vrijeme2` time NOT NULL,
-  `datum1` date NOT NULL,
-  `datum2` date NOT NULL,
-  `datum3` date NOT NULL,
-  `datum4` date NOT NULL,
-  `datum5` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `vrijemefilm`
---
-
-INSERT INTO `vrijemefilm` (`movieID`, `vrijeme`, `vrijeme2`, `datum1`, `datum2`, `datum3`, `datum4`, `datum5`) VALUES
-(1, '12:00:00', '15:00:00', '2021-02-07', '2021-02-08', '2021-02-10', '2021-02-12', '2021-02-13'),
-(2, '19:15:00', '22:00:00', '2021-02-09', '2021-02-11', '2021-02-12', '2021-02-13', '2021-02-16'),
-(3, '16:00:00', '20:00:00', '2021-02-11', '2021-02-13', '2021-02-16', '2021-02-17', '2021-02-19'),
-(12, '12:00:00', '16:00:00', '2021-02-08', '2021-02-09', '2021-02-12', '2021-02-13', '2021-02-18'),
-(14, '18:00:00', '21:00:00', '2021-02-10', '2021-02-11', '2021-02-12', '2021-02-13', '2021-02-15'),
-(15, '12:00:00', '15:00:00', '2021-02-07', '2021-02-08', '2021-02-10', '2021-02-12', '2021-02-13'),
-(17, '19:15:00', '22:00:00', '2021-02-09', '2021-02-11', '2021-02-12', '2021-02-13', '2021-02-16'),
-(18, '16:00:00', '20:00:00', '2021-02-11', '2021-02-13', '2021-02-16', '2021-02-17', '2021-02-19'),
-(21, '17:00:00', '20:30:00', '2021-02-01', '2021-02-03', '2021-02-19', '2021-02-27', '2021-03-03'),
-(22, '19:30:00', '22:15:00', '2021-03-01', '2021-03-03', '2021-03-06', '2021-03-08', '2021-03-10'),
-(23, '21:15:00', '23:00:00', '2021-03-10', '2021-03-09', '2021-03-08', '2021-03-07', '2021-03-06'),
-(25, '17:00:00', '20:30:00', '2021-03-01', '2021-02-03', '2021-03-08', '2021-03-07', '2021-03-10'),
-(26, '21:20:00', '19:20:00', '2021-03-17', '2021-03-18', '2021-03-19', '2021-03-20', '2021-03-21');
+(36, 2, 'Joker', 'A2', '2021-02-17', '22:52:00', '4D'),
+(37, 28, 'Cudo u celiji br 7', 'Mala', '2021-02-24', '16:00:00', '4D');
 
 --
 -- Indexes for dumped tables
@@ -246,7 +206,7 @@ ALTER TABLE `prikazivanje`
 -- AUTO_INCREMENT for table `bookingtable`
 --
 ALTER TABLE `bookingtable`
-  MODIFY `bookingID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+  MODIFY `bookingID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT for table `feedbacktable`
@@ -264,13 +224,13 @@ ALTER TABLE `korisnik`
 -- AUTO_INCREMENT for table `movietable`
 --
 ALTER TABLE `movietable`
-  MODIFY `movieID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `movieID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `prikazivanje`
 --
 ALTER TABLE `prikazivanje`
-  MODIFY `idPrikazivanje` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `idPrikazivanje` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
